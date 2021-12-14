32d51cbe45df2f69d7792fde3f75459c4af1ab24
Klaskovilte <54354641+Klaskovilte@users.noreply.github.com>
2021-12-14 09:57:53 +0100

    Add project files.

---
+++ TEB.HelloWorld/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace TEB.HelloWorld
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
